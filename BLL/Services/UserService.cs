c26f7c63312d4fa0ce2ec54733c975c6955452d3
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-29 12:15:11 +0100

    Add Demo : ASP.net MVC - BLL

---
+++ BLL/Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using BLL.Entities;
+using BLL.Mappers;
+using D = DAL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class UserService
+    {
+        private D.UserService _service;
+
+        public UserService()
+        {
+            _service = new D.UserService();    
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return _service.Get().Select(dal => dal.ToBLL());
+        }
+
+        public User Get(Guid user_id) { 
+            return _service.Get(user_id).ToBLL();
+        }
+
+        public Guid Insert(User user)
+        {
+            return _service.Insert(user.ToDAL());
+        }
+
+        public void Update(Guid user_id, User user)
+        {
+            _service.Update(user_id, user.ToDAL());
+        }
+
+        public void Delete(Guid user_id) {
+            _service.Delete(user_id);
+        }
+
+        public Guid CheckPassword(string email, string password)
+        {
+            return _service.CheckPassword(email, password);
+        }
+    }
+}
--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,5 +1,5 @@
-﻿using DAL.Entities;
-using DAL.Services;
+﻿using BLL.Entities;
+using BLL.Services;
 
 namespace ConsoleTest
 {
@@ -8,9 +8,16 @@ namespace ConsoleTest
         static void Main(string[] args)
         {
             UserService service = new UserService();
+            service.Delete(Guid.Parse("70941061-ed03-444c-9649-1b9fa8a5503f"));
             foreach (User u in service.Get())
             {
+                if (u.IsDisabled)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
                 Console.WriteLine($"{u.User_Id} : {u.First_Name} {u.Last_Name} - {u.Email} : {u.Password}");
+                Console.ResetColor();
             }
         }
     }
