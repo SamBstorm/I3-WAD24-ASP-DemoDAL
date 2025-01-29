c1fdf6dce187685163c8c8d9e6e5450859e35115
Samuel Legrain <samuel.legrain@bstorm.be>
2025-01-29 10:15:33 +0100

    Add Demo : ASP.net MVC (ConsoleTest)

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using DAL.Services;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserService service = new UserService();
+            foreach (User u in service.Get())
+            {
+                Console.WriteLine($"{u.User_Id} : {u.First_Name} {u.Last_Name} - {u.Email} : {u.Password}");
+            }
+        }
+    }
+}
