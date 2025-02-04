2026453f7fba59b543721ce243e59b833dce3c32
Samuel Legrain <samuel.legrain@bstorm.be>
2025-02-04 13:23:05 +0100

    Add Demo - Session Cookie Consent

--- ASP-MVC/Program.cs
+++ ASP-MVC/Program.cs
@@ -11,6 +11,22 @@ namespace ASP_MVC
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
+            //Ajout d'implémentation des services nécessaires à l'utilisation de session :
+            builder.Services.AddDistributedMemoryCache();
+            builder.Services.AddSession(
+                options => {
+                    options.Cookie.Name = "CookieWad24";
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.IsEssential = true;
+                    options.IdleTimeout = TimeSpan.FromMinutes(10);
+                });
+            builder.Services.Configure<CookiePolicyOptions>(options => { 
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+                options.Secure = CookieSecurePolicy.Always;
+            });
+
+
             //Ajout de nos services : Ceux de la BLL et ceux de la DAL
             builder.Services.AddScoped<IUserRepository<BLL.Entities.User>, BLL.Services.UserService>();
             builder.Services.AddScoped<IUserRepository<DAL.Entities.User>, DAL.Services.UserService>();
@@ -26,6 +42,9 @@ namespace ASP_MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSession();
+            app.UseCookiePolicy();
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
